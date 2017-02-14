8740e9e44bfd8ae6494281ea73dd5b7e4fb37b6d
lmy <635875785@qq.com>
2017-02-14 10:01:19 +0800

    Bmob初始化

--- app/src/main/java/com/lmy/anybuy/MainActivity.java
+++ app/src/main/java/com/lmy/anybuy/MainActivity.java
@@ -3,11 +3,18 @@ package com.lmy.anybuy;
 import android.app.Activity;
 import android.os.Bundle;
 
+import cn.bmob.v3.Bmob;
+
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //第一：默认初始化
+        Bmob.initialize(this, "7b802caecd996887785382021ac31877");
+
         setContentView(R.layout.activity_main);
+
     }
 }
---
+++ app/src/main/java/com/lmy/anybuy/logic/test.java
@@ -0,0 +1,11 @@
+package com.lmy.anybuy.logic;
+
+/**
+ * ClassName:
+ * Function:
+ * Author: lmy
+ * Date: 2017/2/14
+ */
+
+public class test {
+}
